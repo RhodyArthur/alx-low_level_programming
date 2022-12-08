78d372b674f70921472ee2f442ece456d66fc643
RhodyArthur <rhodaarthur48@gmail.com>
2022-12-08 15:16:26 -0800

    Print using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#/bin/bash
+
+#include <stdio.h>
+/**
+ * main - prints the string using puts function
+ *
+ * Description: This program prints
+ * "Programming is like building a multilingual puzzle
+ * using puts function
+ * Return: 0
+ * */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

fc9e4cd0e3b630774340b09c480709b9e96a0fde
RhodyArthur <rhodaarthur48@gmail.com>
2022-12-09 09:15:33 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print for statement
+ *
+ * Description: using main function
+ * this program prints numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

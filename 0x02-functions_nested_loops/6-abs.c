a4e44dbe483ce29db9ee612f467adc65728a1393
RhodyArthur <rhodaarthur48@gmail.com>
2022-12-13 10:35:01 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * @n: input character
+ * Description: returns the absolute
+ * value of a number
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}

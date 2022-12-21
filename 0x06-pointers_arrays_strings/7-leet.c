ea20f39bb47db2a0b044c87fc943cd937ff45fc0
RhodyArthur <rhodaarthur48@gmail.com>
2022-12-21 12:14:37 -0800

    Encode a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - function encodes a string
+ * @str: array
+ * Return: array
+ */
+
+char *leet( char  *str)
+{
+	int i, j;
+	char s[] = "aAeEoOtTlL";
+	char s1[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (s[j] == str[i])
+			{
+				str[i] = s1[j];
+			}
+		}
+	}
+	return (str);
+}	

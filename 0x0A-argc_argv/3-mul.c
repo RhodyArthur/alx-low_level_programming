331be830133862285e3b4cbc6ce89e53fed0994d
RhodyArthur <rhodaarthur48@gmail.com>
2023-01-09 18:28:43 -0800

    Multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - multiples two numbers
+ * @argc: arguments
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+
+	if (argc == 3)
+	{
+		mul *= atoi(argv[argv - 2]);
+		mul *= atoi(argv[argv - 1]);
+		printf("%d\n", mul);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}

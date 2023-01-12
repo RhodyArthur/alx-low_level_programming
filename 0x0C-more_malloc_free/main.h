9703e3105c706f04dc707460c1f317e94a229232
RhodyArthur <rhodaarthur48@gmail.com>
2023-01-12 05:47:47 -0800

    Main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 void *malloc_checked(unsigned int b);
 char *string_noncat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);

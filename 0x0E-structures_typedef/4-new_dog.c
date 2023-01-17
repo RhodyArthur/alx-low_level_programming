af3f2b9c0fa0397f30def8f42971daed007c358a
RhodyArthur <rhodaarthur48@gmail.com>
2023-01-16 16:19:32 -0800

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,12 +5,12 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 
 /**
- * new_dog - ...
- * @name: ...
- * @age: ...
- * @owner: ...
+ * new_dog - function
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  *
- * Return: ...
+ * Return: 0
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {

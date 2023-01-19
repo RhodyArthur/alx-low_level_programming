8bd978c0e0aa398e53a96dfec5144b7c85a53d72
RhodyArthur <rhodaarthur48@gmail.com>
2023-01-19 14:57:18 -0800

    Header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,24 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct format - datatype of format
+ * @op: format
+ * @f: function
+ */
+
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} f;

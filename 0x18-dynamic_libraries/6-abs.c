07d1de0cb5afd4ad4185ea49a036fdc5b02dca0a
RhodyArthur <rhodaarthur48@gmail.com>
2023-03-27 12:55:32 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - entry point
+ * @n: input character
+ * Description: returns the absolute
+ * value of a number
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
